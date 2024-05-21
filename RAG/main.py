426c001f78307bc9dd3530880da1785d78f9ac06
Jaacky-Huaang <104845011+Jaacky-Huaang@users.noreply.github.com>
2024-05-21 16:04:47 -0400

    Add files via upload

---
+++ RAG/main.py
@@ -0,0 +1,86 @@
+import os
+import sys
+from dotenv import load_dotenv
+from langchain.chains import ConversationalRetrievalChain
+from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import Docx2txtLoader
+from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores import Chroma
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.llms import HuggingFaceHub
+from langchain_community.embeddings import HuggingFaceEmbeddings
+import pdb
+
+load_dotenv('.env')
+
+documents = []
+# Create a List of Documents from all of our files in the ./docs folder
+for file in os.listdir("docs"):
+    if file.endswith(".pdf"):
+        pdf_path = "./docs/" + file
+        loader = PyPDFLoader(pdf_path)
+        documents.extend(loader.load())
+    elif file.endswith('.docx') or file.endswith('.doc'):
+        doc_path = "./docs/" + file
+        loader = Docx2txtLoader(doc_path)
+        documents.extend(loader.load())
+    elif file.endswith('.txt'):
+        text_path = "./docs/" + file
+        loader = TextLoader(text_path)
+        documents.extend(loader.load())
+    # TODO: Add more document types
+
+# Split the documents into smaller chunks
+text_splitter = RecursiveCharacterTextSplitter(
+    separators=["\n\n", "\n", " ", ""],
+    chunk_size=1000,
+    chunk_overlap=50,
+    length_function=len
+)
+chunks = text_splitter.split_documents(documents)
+
+huggingface_embeddings = HuggingFaceEmbeddings(model_name="BAAI/bge-large-en-v1.5")
+openai_embeddings = OpenAIEmbeddings()
+
+# Convert the document chunks to embedding and save them to the vector store
+vectordb = Chroma.from_documents(chunks, embedding=openai_embeddings, persist_directory="./data")
+vectordb.persist()
+
+# TODO: change the prompt of llm
+
+
+#huggingface_llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+openai_llm = ChatOpenAI(temperature=0.7, model_name='gpt-4o')
+
+# create our Q&A chain
+pdf_qa = ConversationalRetrievalChain.from_llm(
+    openai_llm,
+    retriever=vectordb.as_retriever(search_kwargs={'k': 6}),
+    return_source_documents=True,
+    verbose=False
+)
+
+yellow = "\033[0;33m"
+green = "\033[0;32m"
+white = "\033[0;39m"
+
+chat_history = []
+print(f"{yellow}---------------------------------------------------------------------------------")
+print('Welcome to the DocBot. You are now ready to start interacting with your documents')
+print('---------------------------------------------------------------------------------')
+while True:
+    query = input(f"{green}Prompt: ")
+    # query = "Jacky is a Computer Science student who has taken the following courses:\
+    #         Introduction to Computer Science, Data Structures, Algorithms, Operating Systems, and Computer Networks.\
+    #         Has he completed the requirements for the Computer Science major?\
+    #         If not, what courses does he still need to take?"
+    if query == "exit" or query == "quit" or query == "q" or query == "f":
+        print('Exiting')
+        sys.exit()
+    if query == '':
+        continue
+    result = pdf_qa.invoke({"question": query, "chat_history": chat_history})
+    print(f"{white}Answer: " + result["answer"])
+    chat_history.append((query, result["answer"]))
